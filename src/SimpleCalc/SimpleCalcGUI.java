cc3841a8981a2c00f1a07c26d7d2e3b8e6ed2b44
Ritchel Mendaros <ritchel.mendaros@cit.edu>
2022-12-17 00:34:50 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -9,6 +11,38 @@ public class SimpleCalcGUI extends JFrame {
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                compute();
+            }
+        });
+    }
+
+    public void compute() {
+        String op = (String) cbOperations.getSelectedItem();
+        double n1 = Double.parseDouble(tfNumber1.getText());
+        double n2 = Double.parseDouble(tfNumber2.getText());
+        double res = 0;
+        if(op == "+") {
+            res = n1 + n2;
+            lblResult.setText(String.valueOf(String.format("%.0f", (res))));
+        }
+        if(op == "-") {
+            res = n1 - n2;
+            lblResult.setText(String.valueOf(String.format("%.0f", (res))));
+        }
+        if(op == "*") {
+            res = n1 * n2;
+            lblResult.setText(String.valueOf(String.format("%.0f", (res))));
+        }
+        if(op == "/") {
+            res = n1 / n2;
+            lblResult.setText(String.valueOf(String.format("%.0f", (res))));
+        }
+    }
 
     public static void main(String[] args) {
         SimpleCalcGUI calc = new SimpleCalcGUI();
