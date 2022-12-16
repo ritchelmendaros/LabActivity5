243317b01aa5728eb5c251135e6756fa57ad79c9
Ritchel Mendaros <ritchel.mendaros@cit.edu>
2022-12-17 01:17:19 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
@@ -15,6 +17,54 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+    public FoodOrderGUI() {
+
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                order();
+            }
+        });
+    }
+
+    public void order() {
+        double sum = 0;
+        double total = 0;
+        if(cPizza.isSelected()) {
+            sum += 100;
+        }
+        if(cBurger.isSelected()) {
+            sum += 80;
+        }
+        if(cFries.isSelected()) {
+            sum += 65;
+        }
+        if(cSoftDrinks.isSelected()) {
+            sum += 55;
+        }
+        if(cTea.isSelected()) {
+            sum += 50;
+        }
+        if(cSundae.isSelected()) {
+            sum += 40;
+        }
+        if(rbNone.isSelected()) {
+            sum = sum;
+        }
+        if(rb5.isSelected()) {
+            total = sum * 0.05;
+            sum = sum - total;
+        }
+        if(rb10.isSelected()) {
+            total = sum * 0.10;
+            sum = sum - total;
+        }
+        if(rb15.isSelected()) {
+            total = sum * 0.15;
+            sum = sum - total;
+        }
+        JOptionPane.showMessageDialog(panel1, "The total price is Php " + String.format("%.2f", sum));
+    }
 
     public static void main(String[] args) {
         FoodOrderGUI fo = new FoodOrderGUI();
