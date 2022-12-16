07ba605fe8cad281ed48eb479c73a1a177a346ac
Ritchel Mendaros <ritchel.mendaros@cit.edu>
2022-12-17 02:10:55 +0800

    Add Exception Handling to FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,10 +1,14 @@
 package FoodOrdering;
 
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class FoodOrderGUI extends JFrame {
+public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
@@ -17,6 +21,7 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+    private List<JCheckBox> bgFood;
     public FoodOrderGUI() {
 
         btnOrder.addActionListener(new ActionListener() {
@@ -28,42 +33,62 @@ public class FoodOrderGUI extends JFrame {
     }
 
     public void order() {
-        double sum = 0;
-        double total = 0;
-        if(cPizza.isSelected()) {
-            sum += 100;
+        try {
+            boolean select1 = false;
+            boolean select2 = false;
+            double sum = 0;
+            double total = 0;
+            if (cPizza.isSelected()) {
+                sum += 100;
+                select1 = true;
+            }
+            if (cBurger.isSelected()) {
+                sum += 80;
+                select1 = true;
+            }
+            if (cFries.isSelected()) {
+                sum += 65;
+                select1 = true;
+            }
+            if (cSoftDrinks.isSelected()) {
+                sum += 55;
+                select1 = true;
+            }
+            if (cTea.isSelected()) {
+                sum += 50;
+                select1 = true;
+            }
+            if (cSundae.isSelected()) {
+                sum += 40;
+                select1 = true;
+            }
+            if (rbNone.isSelected()) {
+                sum = sum;
+                select2 = true;
+            }
+            if (rb5.isSelected()) {
+                total = sum * 0.05;
+                sum = sum - total;
+                select2 = true;
+            }
+            if (rb10.isSelected()) {
+                total = sum * 0.10;
+                sum = sum - total;
+                select2 = true;
+            }
+            if (rb15.isSelected()) {
+                total = sum * 0.15;
+                sum = sum - total;
+                select2 = true;
+            }
+            if(select2 == false || select1 == false) {
+                throw new IllegalArgumentException("No discount or food selected");
+            }
+            JOptionPane.showMessageDialog(panel1, "The total price is Php " + String.format("%.2f", sum));
         }
-        if(cBurger.isSelected()) {
-            sum += 80;
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
-        if(cFries.isSelected()) {
-            sum += 65;
-        }
-        if(cSoftDrinks.isSelected()) {
-            sum += 55;
-        }
-        if(cTea.isSelected()) {
-            sum += 50;
-        }
-        if(cSundae.isSelected()) {
-            sum += 40;
-        }
-        if(rbNone.isSelected()) {
-            sum = sum;
-        }
-        if(rb5.isSelected()) {
-            total = sum * 0.05;
-            sum = sum - total;
-        }
-        if(rb10.isSelected()) {
-            total = sum * 0.10;
-            sum = sum - total;
-        }
-        if(rb15.isSelected()) {
-            total = sum * 0.15;
-            sum = sum - total;
-        }
-        JOptionPane.showMessageDialog(panel1, "The total price is Php " + String.format("%.2f", sum));
     }
 
     public static void main(String[] args) {
@@ -74,5 +99,4 @@ public class FoodOrderGUI extends JFrame {
         fo.setVisible(true);
         fo.setTitle("Food Ordering System");
     }
-
 }
